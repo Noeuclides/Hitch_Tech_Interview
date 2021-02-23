90786b3371532c08071cbdd239109f32fbeaf4ac
Nicolás <euclidesnoeuclides@gmail.com>
2021-02-23 01:31:32 -0500

    pycodestyle

--- hitch_api/nba_api/api/views.py
+++ hitch_api/nba_api/api/views.py
@@ -1,27 +1,33 @@
 from .base_views import GeneralListApiView
 from rest_framework import generics
 from .serializers import (
-    PlayerSerializer, TeamSerializer, PositionSerializer, CollegeSerializer, PlayerCreationSerializer
+    PlayerSerializer, TeamSerializer, PositionSerializer,
+    CollegeSerializer, PlayerCreationSerializer
 )
+
 
 class PlayerListAPIView(GeneralListApiView):
     serializer_class = PlayerSerializer
 
+
 class PlayerCreateAPIView(generics.CreateAPIView):
     serializer_class = PlayerCreationSerializer
+
 
 class PlayerRetrieveUpdateDestroyApiView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PlayerCreationSerializer
 
-    def get_queryset(self,pk=None):
+    def get_queryset(self, pk=None):
         return self.get_serializer().Meta.model.objects.all()
 
 
 class TeamListAPIView(GeneralListApiView):
     serializer_class = TeamSerializer
 
+
 class PositionListAPIView(GeneralListApiView):
     serializer_class = PositionSerializer
+
 
 class CollegeListAPIView(GeneralListApiView):
     serializer_class = CollegeSerializer
